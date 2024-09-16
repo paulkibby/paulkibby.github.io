e5217cb3646e3a2f35bb6cf85c65d202fbbea225
Paul Kibby <kibby.ru@gmail.com>
2024-09-16 15:50:15 +0400

    Create what.js

---
+++ public/js/what.js
@@ -0,0 +1,41 @@
+function r9KrDvvJirQhTMLwR6uRwtPQiH106sVC31CpzRpped() {
+    const stop = document.querySelector('.stop');
+    stop.classList.remove('stop-hidden');
+
+    setTimeout(() => {
+        stop.classList.add('stop-hidden');
+    }, 5000);
+
+    console.log('%c13. STOP IT.','font-size: 64px; color: #F62A31;');
+    return '                              .                              ';
+}
+
+console.log(
+        `%c
+                   ┬┴┬┴┤(･_├┬┴┬┴                  
+        
+%c                                                  
+%c   r9KrDvvJirQhTMLwR6uRwtPQiH106sVC31CpzRpped()   
+%c    _  ___ _     _                                
+   | |/ (_) |__ | |__  _   _                      
+   | ' /| | '_ \\| '_ \\| | | |                     
+   | . \\| | |_) | |_) | |_| |                     
+   |_|\\_\\_|_.__/|_.__/ \\__, |                     
+                       |___/                      
+                                                  
+%c   r9KrDvvJirQhTMLwR6uRwtPQiH106sVC31CpzRpped()   
+                                                  
+   ---                                      ---   
+%c   Hello.                                         
+   You found this place.                          
+%c   ---                                      ---   
+%c                                                  `,
+        'font-size: 13px; color: #808183;',
+        'font-size: 13px; background: #18191E; color: #808183; border-radius: 16px 16px 0px 0px',
+        'font-size: 13px; background: #18191E; color: #808183',
+        'font-size: 13px; background: #18191E; color: #FFFFFF',
+        'font-size: 13px; background: #18191E; color: #808183',
+        'font-size: 13px; background: #18191E; color: #FFFFFF',
+        'font-size: 13px; background: #18191E; color: #808183',
+        'font-size: 13px; background: #18191E; color: #808183; border-radius: 0px 0px 16px 16px'
+);
